b5f6b5c9d4c0cc7e1b077946144f09ccd82070d6
Nicholas Patterson <nicholaspatterson36@outlook.com>
2021-03-12 20:00:40 -0500

    hero section animation complete

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,19 +7,13 @@ export default function Hero() {
             <div className={HeroCSS.sectionNumber}>
                 1
             </div>
-            <div>
+            <div className={HeroCSS.sectionInfo}>
                 <span>Hi, my name is</span>
-            </div>
-            <div>
                 <h1>Nicholas Patterson.</h1>
-            </div>
-            <div>
                 <h2>I build cool things for the web.</h2>
-            </div>
-            <div className={HeroCSS.copy}>
                 <p>I'm a software developer based in Pennsylvania, specializing in frontend development. </p>
-            </div>
                 <button className={HeroCSS.button} type="button">Resume</button>
+            </div>
         </section>
     )
 }
